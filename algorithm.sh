d944f69599f3fcf6a799b2737f2c45808d48f523
Yauhen Viazau <Yauhen_Viazau@epam.com>
2023-12-16 00:48:21 +0500

    Delete algorithm.sh

--- algorithm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee report.txt
-# echo "3.14159" | tee -a report.txt
-
-# more precise value
-echo "MORE PRECISE VALUE OF PI:"
-echo "scale=1000; 4*a(1)" | bc -l  | sed 's/.$//' | tr -d '\n'| tee report.txt
